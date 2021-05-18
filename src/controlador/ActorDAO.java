a004375db42c6e7c8aaf27f5c1eadf41c807cff5
Miguel <Miguel@LAPTOP-TUIEGDGS>
2021-05-18 01:37:48 -0500

    Se añadio metodos de insercion, actualizacion y eliminacion a la clase ActorDAO

--- src/controlador/ActorDAO.java
+++ src/controlador/ActorDAO.java
@@ -4,11 +4,41 @@
  * and open the template in the editor.
  */
 package controlador;
+import conexion.ConexionBD;
+import java.sql.*;
+import modelo.Actor;
+import modelo.Film;
 
 /**
  *
  * @author Miguel
  */
-public class ActorDAO {
+public class ActorDAO extends Actor{
+    
+    public static void EntradaActor(String nom,String cat)throws SQLException{
+        CallableStatement agr =conexion.ConexionBD.getConnection().prepareCall("select insertaactor(?,?)");
+        agr.setString(1, nom);
+        agr.setString(2, cat);
+        agr.execute();
+        
+    }
+    
+    
+    public boolean modificarActor(Actor a) {
+    			String sql = "UPDATE actor SET actor_name='"+a.getNombre()+"',"
+                                             +"actor_last_name='"+a.getApellido()
+    					     +"' WHERE actor_id ='"+a.getId()+"'";
+    			boolean res = new ConexionBD().ejecutarInstruccion1(sql);
+    			
+    			return res; 
+                        
+    }         
+    
+    public static void EliminarActor(int id)throws SQLException{
+        CallableStatement a =conexion.ConexionBD.getConnection().prepareCall("select ea(?)");
+        a.setInt(1, id);
+        a.execute();
+        
+    }
     
 }
